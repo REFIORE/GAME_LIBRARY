6e11bd0916ba283a91dff976e06f20cdc2ddb678
REFIORE <aras_andrey@bk.ru>
2025-10-18 20:05:08 +0300

    Поправил структуру кода

--- main.py
+++ main.py
@@ -3,7 +3,38 @@ import requests
 from dotenv import load_dotenv
 
 
-def get_games():
+def get_games(payload):
+    url = 'https://api.rawg.io/api/games'
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    games = response.json()['results']
+    return games
+
+
+def get_screenshot(payload, slug):
+    screenshots_url = f'https://api.rawg.io/api/games/{slug}/screenshots'
+    response = requests.get(screenshots_url, params=payload)
+    response.raise_for_status()
+    sceens = response.json()['results']
+    print('Скриншоты из игры: ')
+    for screen in sceens:
+        screenshot = screen['image']
+        print(screenshot)
+
+
+def get_sells(payload, slug):
+    sell_url = f'https://api.rawg.io/api/games/{slug}/stores'
+    response = requests.get(sell_url, params=payload)
+    response.raise_for_status()
+    sells = response.json()['results']
+    print('Где можно приобрести: ')
+    for sell in sells:
+        sell_urls = sell['url']
+        print(sell_urls)
+
+
+def main():
+    load_dotenv()
     token = os.getenv('API_KEY')
     payload = {
         'key': token,
@@ -12,11 +43,7 @@ def get_games():
         'metacritic': 100,
         'tags': 'multiplayer',
     }
-    url = 'https://api.rawg.io/api/games'
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-    games = response.json()['results']
-
+    games = get_games(payload)
     for game in games:
         slug = game['slug']
         game_name = game['name']
@@ -25,29 +52,8 @@ def get_games():
         print(f'\nНазвание игры: {game_name}')
         print(f'Дата выхода: {date_released}')
         print(f'Ссылка на игру: {game_url}')
-
-        screenshots_url = f'https://api.rawg.io/api/games/{slug}/screenshots'
-        response = requests.get(screenshots_url, params=payload)
-        response.raise_for_status()
-        sceens = response.json()['results']
-        print('Скриншоты из игры: ')
-        for screen in sceens:
-            screenshot = screen['image']
-            print(screenshot)
-
-        sell_url = f'https://api.rawg.io/api/games/{slug}/stores'
-        response = requests.get(sell_url, params=payload)
-        response.raise_for_status()
-        sells = response.json()['results']
-        print('Где можно приобрести: ')
-        for sell in sells:
-            sell_urls = sell['url']
-            print(sell_urls)
-
-
-def main():
-    load_dotenv()
-    get_games()
+        get_screenshot(payload, slug)
+        get_sells(payload, slug)
 
 
 if __name__ == "__main__":
