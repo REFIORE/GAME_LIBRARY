efbcb8adb5dbcbd78623fb549e0c06c418a82b0a
REFIORE <aras_andrey@bk.ru>
2025-10-18 18:35:37 +0300

    Вывод в терминал названия, даты и ссылки игры

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ load_dotenv()
 token = os.getenv('API_KEY')
 payload = {
     'key': token,
+    'genres': 'strategies',
 }
 url = 'https://api.rawg.io/api/games'
 response = requests.get(url, params=payload)
 response.raise_for_status()
-response = response.json()
+games = response.json()['results']
 
-game_name = response['results']['name']
-
-print('Название игры: ', game_name)
+for game in games:
+    slug = game['slug']
+    game_name = game['name']
+    game_url = f'https://rawg.io/games/{slug}'
+    date_released = game['released']
+    print(f'Название игры: {game_name}')
+    print(f'Дата выхода: {date_released}')
+    print(f'Ссылка на игру: {game_url}\n')
