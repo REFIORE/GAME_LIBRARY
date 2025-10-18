d017c436987975cc9a6deb5d1c8bcb5d5a2162d2
REFIORE <aras_andrey@bk.ru>
2025-10-18 19:03:54 +0300

    if __name__ == "__main__":

--- main.py
+++ main.py
@@ -3,22 +3,27 @@ import requests
 from dotenv import load_dotenv
 
 
-load_dotenv()
-token = os.getenv('API_KEY')
-payload = {
-    'key': token,
-    'genres': 'strategies',
-}
-url = 'https://api.rawg.io/api/games'
-response = requests.get(url, params=payload)
-response.raise_for_status()
-games = response.json()['results']
+def main():
+    load_dotenv()
+    token = os.getenv('API_KEY')
+    payload = {
+        'key': token,
+        'genres': 'strategy'
+    }
+    url = 'https://api.rawg.io/api/games'
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    games = response.json()['results']
 
-for game in games:
-    slug = game['slug']
-    game_name = game['name']
-    game_url = f'https://rawg.io/games/{slug}'
-    date_released = game['released']
-    print(f'Название игры: {game_name}')
-    print(f'Дата выхода: {date_released}')
-    print(f'Ссылка на игру: {game_url}\n')
+    for game in games:
+        slug = game['slug']
+        game_name = game['name']
+        game_url = f'https://rawg.io/games/{slug}'
+        date_released = game['released']
+        print(f'Название игры: {game_name}')
+        print(f'Дата выхода: {date_released}')
+        print(f'Ссылка на игру: {game_url}\n')
+
+
+if __name__ == "__main__":
+    main()
